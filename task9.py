93a64fcc46860bc370e30e649224d115b0b8f2ed
nastya24704 <nastazeravina@gmail.com>
2025-02-08 15:27:39 +0700

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,6 +1,8 @@
+print('Введите плей-лист папы:')
 song1=input()
 song2=input()
 song3=input()
 song4=input()
 song5=input()
+print('Плей-лист мамы:')
 print(song5, song4, song3, song2, song1, sep='\n')
